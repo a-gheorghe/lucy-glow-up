f8cccfe2d9071e9d368500ece2752d0618bf5aed
Ana-Stefania Gheorghe <agheorghe@Ana-Stefanias-MacBook-Pro.local>
2021-04-11 22:43:48 +0200

    show add forms only if user is admin

--- src/components/Dogs/GrowthPictures/index.js
+++ src/components/Dogs/GrowthPictures/index.js
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import { GrowthPicturesForm } from "./GrowthPicturesForm";
 import { Timeline } from "./Timeline";
 import firebase from "../../../firebase/clientApp";
+import { useUser } from "../../../contexts/userContext";
+import { isAdmin } from "../../../utils/isAdmin";
 
 const GrowthPicturesBase = () => {
+  const user = useUser().user;
   const [pictures, setPictures] = useState([]);
 
   useEffect(() => {
@@ -22,7 +25,7 @@ const GrowthPicturesBase = () => {
 
   return (
     <>
-      <GrowthPicturesForm />
+      {isAdmin(user) && <GrowthPicturesForm />}
       <div>
         Month: <a href="#March-2021">March 2021</a>
       </div>
