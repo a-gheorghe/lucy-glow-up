de46c97788cd05398740e4f019dad1a34a0484b4
Ana-Stefania Gheorghe <agheorghe@Ana-Stefanias-MacBook-Pro.local>
2021-05-24 08:21:33 +0200

    Bring froms into their own routes

--- src/components/Dogs/GrowthWeight/index.js
+++ src/components/Dogs/GrowthWeight/index.js
@@ -1,11 +1,19 @@
 import React, { useEffect, useState } from "react";
+import { useHistory } from "react-router-dom";
 import styled from "styled-components";
 import dayjs from "dayjs";
-import { GrowthWeightForm } from "./GrowthWeightForm";
 import { LineChart } from "./LineChart";
+import * as ROUTES from "../../../constants/routes";
+
+import AddIcon from "@material-ui/icons/Add";
+import Fab from "@material-ui/core/Fab";
 import firebase from "../../../firebase/clientApp";
 
 const GrowthWeightBase = (props) => {
+  const history = useHistory();
+  const navigateToForm = () => {
+    history.push(ROUTES.LUCY_GROWTH_WEIGHT_ADD);
+  };
   const ChartContainer = styled.div`
     & svg {
       overflow: unset !important;
@@ -29,7 +37,19 @@ const GrowthWeightBase = (props) => {
 
   return (
     <>
-      <GrowthWeightForm />
+      <Fab
+        color="primary"
+        aria-label="add"
+        style={{
+          position: "fixed",
+          bottom: "30px",
+          right: "30px",
+          zIndex: "100",
+        }}
+        onClick={navigateToForm}
+      >
+        <AddIcon />
+      </Fab>
       <ChartContainer>
         <LineChart
           data={weights.map((weight) => ({
